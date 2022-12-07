0fc3434fa6764586b56861ba329cbc79285446d6
GOlivierNation <ogasominali@gmail.com>
2022-12-07 15:48:29 -0500

    Add effect on Scroll

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,6 +8,18 @@ export const NavBar = () => {
     const [activeLink, setActiveLink] = useState('home');
     const [scrolled, setScrolled] = useState(false);
 
+    useEffect(() => {
+        const onScroll = () => {
+           if (window.scrollY > 50) {
+               setScrolled(true)
+           } else {
+               setScrolled(false)
+           }
+        }
+        window.addEventListener('scroll', onScroll);
+        return () => window.removeEventListener('scroll', onScroll);
+    }, [])
+
   return (
     <NavBar expand="lg">
         <Container>
